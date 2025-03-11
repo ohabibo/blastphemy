7dcc0fe6cfb454801b38f2253ac81ec79b418988
KendahlRo Turk <kendahl.turk@hva.nl>
2025-03-11 11:14:19 +0100

    Feature made set up for the PlayerAttack

---
+++ game/Content/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,13 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Blok3Game.Engine.GameObjects;
+using Blok3Game.Engine.Helpers;
+using System;
+
+namespace Blok3Game.content.Scripts
+{
+    public class PlayerAttack : GameObject
+    {
+        public PlayerAttack(){}
+     }
+}
