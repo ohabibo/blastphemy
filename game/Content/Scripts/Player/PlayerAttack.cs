92cadf1bf2d6eedf8da97817b3eebcbb4e94cdde
KendahlRo Turk <kendahl.turk@hva.nl>
2025-03-11 19:30:32 +0100

    Feature First internals of player attack

--- game/Content/Scripts/Player/PlayerAttack.cs
+++ game/Content/Scripts/Player/PlayerAttack.cs
@@ -3,11 +3,19 @@ using Microsoft.Xna.Framework.Graphics;
 using Blok3Game.Engine.GameObjects;
 using Blok3Game.Engine.Helpers;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Blok3Game.content.Scripts
 {
     public class PlayerAttack : GameObject
     {
-        public PlayerAttack(){}
-     }
+        private Vector2 direction;
+        private PlayerAttack(Vector2 _direction, Vector2 _velocity)
+        {
+            direction = _direction;
+            velocity = _velocity;
+        }
+        public override void Update(GameTime gameTime)
+        { }
+    }
 }
